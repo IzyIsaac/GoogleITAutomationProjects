9807ebae59df23bfee621e430fdf42873851425f
Isaac <isaac.mauro@gmail.com>
2021-12-26 14:31:35 -0700

    Debug change

--- final_project/supplier_image_upload.py
+++ final_project/supplier_image_upload.py
@@ -7,7 +7,7 @@ url = 'http://34.123.169.96/upload/'
 image_dir = 'supplier-data/images'
 
 for filename in os.listdir(image_dir):
-    if imghdr.what(os.path.join(image_dir, filename)) == 'tiff': continue
+    if imghdr.what(os.path.join(image_dir, filename)) != 'jpeg': continue
     with open(os.path.join(image_dir, filename)) as opened:
         print(f'Posting {os.path.join(image_dir, filename)}')
         r = requests.post(url, files={'file': opened})
