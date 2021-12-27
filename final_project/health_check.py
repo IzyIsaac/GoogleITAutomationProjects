afb4a996813671a5a80f656a01f15a324f26ce7e
isaac <isaac.mauro@gmail.com>
2021-12-27 00:27:20 +0000

    Complete final project

--- final_project/health_check.py
+++ final_project/health_check.py
@@ -7,30 +7,30 @@ import psutil
 import socket
 import emails
 
-class Alert(Enum):
-    CPU = 'Error - CPU usage is over 80%'
-    DISK = 'Error - Available disk space is less than 20%'
-    MEMORY = 'Error - Available memory is less than 500MB'
-    HOST = 'Error - localhost cannot be resolved to 127.0.0.1'
 
 
 
 def main(argv):
+    CPU = 'Error - CPU usage is over 80%'
+    DISK = 'Error - Available disk space is less than 20%'
+    MEMORY = 'Error - Available memory is less than 500MB'
+    HOST = 'Error - localhost cannot be resolved to 127.0.0.1'
     sender = 'automation@example.com'
     recipient = 'student-00-405db269f197@example.com'
     body = 'Please check your system and resolve the issue as soon as possible'
-    cpu_percent = psutil.cpu_percent()
+    cpu_percent = psutil.cpu_percent(interval=1)
+    print(cpu_percent)
     memory_available = psutil.virtual_memory().available
     disk_usage = psutil.disk_usage('/').percent
     localhost = socket.gethostbyname('localhost')
     if cpu_percent > 80:
-        emails.send(emails.generate(sender, recipient, str(Alert.CPU), body))
+        emails.send(emails.generate(sender, recipient, CPU, body))
     if memory_available < 524288000:
-        emails.send(emails.generate(sender, recipient, str(Alert.MEMORY), body))
+        emails.send(emails.generate(sender, recipient, MEMORY, body))
     if disk_usage > 80:
-        emails.send(emails.generate(sender, recipient, str(Alert.DISK), body))
+        emails.send(emails.generate(sender, recipient, DISK, body))
     if localhost != '127.0.0.1':
-        emails.send(emails.generate(sender, recipient, str(Alert.HOST), body))
+        emails.send(emails.generate(sender, recipient, HOST, body))
 
 if __name__ == "__main__":
     main(sys.argv)
